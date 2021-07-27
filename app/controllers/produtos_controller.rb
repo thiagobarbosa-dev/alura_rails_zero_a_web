c0cfc05da8ae2b22cff409c553780d171a536987
Thiago Barbosa <thiagobarbosa48@gmail.com>
2021-07-27 17:10:40 -0300

    Layouts

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,4 +4,9 @@ class ProdutosController < ApplicationController
     @produtos_por_nome = Produto.order(:nome).limit 5
     @produtos_por_preco = Produto.order(:preco).limit 2
   end
+  def create
+    valores = params.require(:produto).permit(:nome, :descricao, :quantidade, :preco)
+    produto = Produto.create valores
+
+  end
 end
