144c1cf1b0f965c0885921d46c4be1f5d7caecb7
Thiago Barbosa <thiagobarbosa48@gmail.com>
2021-07-28 10:14:36 -0300

    implementing search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :produtos, only: [:new, :create, :destroy]
+  get "/produtos/busca" => "produtos#busca", as: :busca_produto
   root 'produtos#index'
   # post '/produtos' => 'produtos#create'
   # get '/produtos/new' => 'produtos#new'
